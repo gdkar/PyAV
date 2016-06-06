8488b379b8dc53543bea9581e30fb348ab81f2d4
gabriel d. karpman <gdkar@mit.edu>
2016-06-05 22:59:04 -0400

    hmmm....?

--- av/plane.pxd
+++ av/plane.pxd
@@ -3,9 +3,8 @@ from av.frame cimport Frame
 
 
 cdef class Plane(Buffer):
-    
     cdef Frame frame
     cdef int index
-
+    cdef readonly size_t buffer_size
     cdef size_t _buffer_size(self)
     cdef void*  _buffer_ptr(self)
--- av/plane.pyx
+++ av/plane.pyx
@@ -6,18 +6,24 @@ cdef class Plane(object):
         self.frame = frame
         self.index = index
         self.buffer_size = frame.ptr.linesize[index]
-    def __repr__(self):    return '<av.%s at 0x%x>' % (self.__class__.__name__, id(self))
-    def __len__(self):     return self.buffer_size
+    def __repr__(self):
+        return '<av.%s at 0x%x>' % (self.__class__.__name__, id(self))
+    def __len__(self):
+        return self.buffer_size
     property line_size:
         """Bytes per horizontal line in this plane."""
-        def __get__(self): return self.frame.ptr.linesize[self.index]
+        def __get__(self):
+            return self.frame.ptr.linesize[self.index]
     property ptr:
-        def __get__(self): return <long>self.frame.ptr.extended_data[self.index]
-    def to_bytes(self):    return <bytes>(<char*>self.frame.ptr.extended_data[self.index])[:self.buffer_size]
+        def __get__(self):
+            return <long>self.frame.ptr.extended_data[self.index]
+    def to_bytes(self):
+        return <bytes>(<char*>self.frame.ptr.extended_data[self.index])[:self.buffer_size]
     def update(self, input):
         """Replace the data in this plane with the given buffer."""
         cdef ByteSource source = bytesource(input)
-        if source.length != self.buffer_size: raise ValueError('got %d bytes; need %d bytes' % (len(input), self.buffer_size))
+        if source.length != self.buffer_size:
+            raise ValueError('got %d bytes; need %d bytes' % (len(input), self.buffer_size))
         memcpy(<void*>self.frame.ptr.extended_data[self.index], source.ptr, self.buffer_size)
     def update_from_string(self, input):
         """Replace the data in this plane with the given string.
@@ -30,16 +36,19 @@ cdef class Plane(object):
     # See: http://docs.python.org/2/c-api/typeobj.html#PyBufferProcs
 
     def __getsegcount__(self, Py_ssize_t *len_out):
-        if len_out != NULL: len_out[0] = <Py_ssize_t>self.buffer_size
+        if len_out != NULL:
+            len_out[0] = <Py_ssize_t>self.buffer_size
         return 1
 
     def __getreadbuffer__(self, Py_ssize_t index, void **data):
-        if index: raise RuntimeError("accessing non-existent buffer segment")
+        if index:
+            raise RuntimeError("accessing non-existent buffer segment")
         data[0] = <void*>self.frame.ptr.extended_data[self.index]
         return <Py_ssize_t>self.buffer_size
 
     def __getwritebuffer__(self, Py_ssize_t index, void **data):
-        if index: raise RuntimeError("accessing non-existent buffer segment")
+        if index:
+            raise RuntimeError("accessing non-existent buffer segment")
         data[0] = <void*>self.frame.ptr.extended_data[self.index]
         return <Py_ssize_t>self.buffer_size
     # New-style buffer support.
