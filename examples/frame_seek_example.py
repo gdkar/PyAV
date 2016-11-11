da4a9ff366c5e246c3b3fcbedbde688958aed6f2
gabriel d. karpman <gdkar@alum.mit.edu>
2016-11-11 06:38:46 -0500

    some example rework

--- examples/frame_seek_example.py
+++ examples/frame_seek_example.py
@@ -1,8 +1,8 @@
 """
-Note this example only really works accurately on constant frame rate media. 
+Note this example only really works accurately on constant frame rate media.
 """
 
-from .qtproxy import Q
+from qtproxy import Q
 import sys
 import av
 
@@ -17,7 +17,7 @@ def get_frame_rate(stream):
     if stream.time_base.denominator and stream.time_base.numerator:
         return 1.0/float(stream.time_base)
     else: raise ValueError("Unable to determine FPS")
-    
+
 def get_frame_count(f, stream):
     if stream.frames:     return stream.frames
     elif stream.duration: return pts_to_frame(stream.duration, float(stream.time_base), get_frame_rate(stream), 0)
@@ -41,7 +41,7 @@ class FrameGrabber(Q.QObject):
         self.last_shown=-1
         self.rate = None
         self.time_base = None
-        
+
     def next_frame(self):
         frame_index = None
         rate = self.rate
@@ -66,14 +66,14 @@ class FrameGrabber(Q.QObject):
         # could use the buffer interface here instead, some versions of PyQt don't support it for some reason
         # need to track down which version they added support for it
         self.frame = bytearray(rgba.planes[0])
-        bytesPerPixel  =3 
+        bytesPerPixel  =3
         img = Q.QImage(self.frame, rgba.width, rgba.height, rgba.width * bytesPerPixel, Q.QImage.Format_RGB888)
-        
+
         #img = QtGui.QImage(rgba.planes[0], rgba.width, rgba.height, QtGui.QImage.Format_RGB888)
 
         #pixmap = QtGui.QPixmap.fromImage(img)
         self.frame_ready.emit(img, target_frame)
-        
+
     def get_frame(self, target_frame):
 
         if target_frame != self.active_frame:return
@@ -107,7 +107,7 @@ class FrameGrabber(Q.QObject):
                     del self.frame_cache[index]
             for i, (frame_index, frame) in enumerate(self.next_frame()):
                 self.frame_cache[frame_index] = frame
-                self.last_seen = frame_index 
+                self.last_seen = frame_index
                 # optimization if the time slider has changed, the requested frame no longer valid
 #                if target_frame != self.active_frame:return
                 print(("   ", i, "at frame", frame_index, "at ts:", frame.pts,frame.dts,"target:", target_pts, 'orig', original_target_frame_pts))
@@ -127,30 +127,27 @@ class FrameGrabber(Q.QObject):
                         print((over_seek, self.frame_cache))
                         break
 
-                    
+
                 seek_frame -= 1
                 reseek -= 1
                 print(("over seeked by %s, backtracking.. seeking: %i target: %i retry: %i" % (str(over_seek),  seek_frame, target_frame, reseek)))
 
             else:
                 break
-        
+
         if reseek < 0:
             raise ValueError("seeking failed %i" % frame_index)
-            
         # frame at this point should be the correct frame
-        
+
         if frame:
-            
             return frame
-        
         else:
             raise ValueError("seeking failed %i" % target_frame)
-        
+
     def get_frame_count(self):
-        
+
         frame_count = None
-        
+
         if self.stream.frames:
             frame_count = self.stream.frames
         elif self.stream.duration:
@@ -159,41 +156,43 @@ class FrameGrabber(Q.QObject):
             frame_count = pts_to_frame(self.file.duration, 1/float(AV_TIME_BASE), get_frame_rate(self.stream), 0)
         else:
             raise ValueError("Unable to determine number for frames")
-        
+
         seek_frame = frame_count
-        
+
         retry = 100
-        
+
         while retry:
             target_sec = seek_frame * 1/ self.rate
             target_pts = int(target_sec / self.time_base) + self.start_time
-            
+
             self.stream.seek(int(target_pts))
-            
+
             frame_index = None
-            
+
             for frame_index, frame in self.next_frame():
                 print((frame_index, frame))
                 continue
-            
+
             if not frame_index is None:
                 break
             else:
-                seek_frame -= 1 
+                seek_frame -= 1
                 retry -= 1
-                
-        
+
+
         print(("frame count seeked", frame_index, "container frame count", frame_count))
-        
+
         return frame_index or frame_count
-    
+
     @Q.pyqtSlot(object)
     def set_file(self, path):
         self.file = av.open(path)
-        self.stream = next(s for s in self.file.streams if s.type == b'video')
+#        for s in filter(lambda x: x.type == 'video',self.file.streams):
+#            self.stream = s
+        self.stream = next(s for s in self.file.streams if s.type == 'video')
         self.rate = get_frame_rate(self.stream)
         self.time_base = float(self.stream.time_base)
-        
+
 
         index, first_frame = next(self.next_frame())
         self.stream.seek(self.stream.start_time)
@@ -205,88 +204,88 @@ class FrameGrabber(Q.QObject):
             pts = first_frame.pts
         else:
             pts = first_frame.dts
- 
+
         self.start_time = pts or first_frame.dts
-            
+
         print(("First pts", pts, self.stream.start_time, first_frame))
 
         #self.nb_frames = get_frame_count(self.file, self.stream)
         self.nb_frames = self.get_frame_count()
-        
+
         self.update_frame_range.emit(self.nb_frames)
-        
+
 class DisplayWidget(Q.QLabel):
     def __init__(self, parent=None):
         super(DisplayWidget, self).__init__(parent)
         #self.setScaledContents(True)
         self.setMinimumSize(1920/10, 1080/10)
-        
+
         size_policy = Q.QSizePolicy(Q.QSizePolicy.Preferred, Q.QSizePolicy.Preferred)
         size_policy.setHeightForWidth(True)
-        
+
         self.setSizePolicy(size_policy)
-        
+
         self.setAlignment(Q.AlignHCenter| Q.AlignBottom)
 
         self.pixmap = None
         self.setMargin(10)
-        
+
     def heightForWidth(self, width):
         return width * 9 / 16.0
-    
+
     @Q.pyqtSlot(object, object)
     def setPixmap(self, img, index):
         #if index == self.current_index:
         self.pixmap = Q.QPixmap.fromImage(img)
-        
+
         #super(DisplayWidget, self).setPixmap(self.pixmap)
         super(DisplayWidget, self).setPixmap(self.pixmap.scaled(self.size(), Q.KeepAspectRatio, Q.SmoothTransformation))
-    
+
     def sizeHint(self):
         width = self.width()
         return Q.QSize(width, self.heightForWidth(width))
-    
+
     def resizeEvent(self, event):
         if self.pixmap:
             super(DisplayWidget, self).setPixmap(self.pixmap.scaled(self.size(), Q.KeepAspectRatio, Q.SmoothTransformation))
-        
+
     def sizeHint(self):
         return Q.QSize(1920/2.5,1080/2.5)
-        
+
 
 class VideoPlayerWidget(Q.QWidget):
-    
+
     request_frame = Q.pyqtSignal(object)
-    
+
     load_file = Q.pyqtSignal(object)
-    
+
     def __init__(self, parent=None):
         super(VideoPlayerWidget, self).__init__(parent)
         self.display = DisplayWidget()
         self.timeline = Q.QScrollBar(Q.Horizontal)
         self.frame_grabber = FrameGrabber()
-        
+
         self.frame_control = Q.QSpinBox()
         self.frame_control.setFixedWidth(100)
 
         self.timeline.valueChanged.connect(self.frame_changed)
         self.frame_control.valueChanged.connect(self.frame_changed)
-        
+
         self.request_frame.connect(self.frame_grabber.request_frame)
         self.load_file.connect(self.frame_grabber.set_file)
 
         self.frame_grabber.frame_ready.connect(self.display.setPixmap)
         self.frame_grabber.update_frame_range.connect(self.set_frame_range)
-        
+
         self.frame_grabber_thread = Q.QThread()
-        
+
         self.frame_grabber.moveToThread(self.frame_grabber_thread)
         self.frame_grabber_thread.start()
-        
+
         control_layout = Q.QHBoxLayout()
         control_layout.addWidget(self.frame_control)
         control_layout.addWidget(self.timeline)
-        
+
         layout = Q.QVBoxLayout()
         layout.addWidget(self.display)
         layout.addLayout(control_layout)
@@ -301,13 +300,13 @@ class VideoPlayerWidget(Q.QWidget):
         #self.frame_grabber.set_file(path)
         self.load_file.emit(path)
         self.frame_changed(0)
-        
+
     @Q.pyqtSlot(object)
     def set_frame_range(self, maximum):
         print(("frame range =", maximum))
         self.timeline.setMaximum(maximum)
         self.frame_control.setMaximum(maximum)
-    
+
     def frame_changed(self, value):
         self.timeline.blockSignals(True)
         self.frame_control.blockSignals(True)
@@ -318,58 +317,58 @@ class VideoPlayerWidget(Q.QWidget):
         #self.display.current_index = value
         self.frame_grabber.active_frame = value
         self.request_frame.emit(value)
-       
+
     def autoTick(self):
         self.timeline.setValue(self.timeline.value()+1)
+
     def keyPressEvent(self, event):
         if event.key() in (Q.Key_Right, Q.Key_Left):
             direction = 1
             if event.key() == Q.Key_Left:
                 direction = -1
-                
+
             if event.modifiers() == Q.ShiftModifier:
                 print('shift')
                 direction *= 10
-                
+
             self.timeline.setValue(self.timeline.value() + direction)
-                
+
         else:
             super(VideoPlayerWidget,self).keyPressEvent(event)
-            
+
     def mousePressEvent(self, event):
         # clear focus of spinbox
         focused_widget = Q.QApplication.focusWidget()
         if focused_widget:
             focused_widget.clearFocus()
-            
+
         super(VideoPlayerWidget,self).mousePressEvent(event)
-        
+
     def dragEnterEvent(self, event):
         event.accept()
-        
+
     def dropEvent(self, event):
-        
+
         mime = event.mimeData()
         event.accept()
-        
-        
+
+
         if mime.hasUrls():
             path = str(mime.urls()[0].path())
             self.set_file(path)
     def closeEvent(self, event):
-        
+
         self.frame_grabber.active_frame = -1
         self.frame_grabber_thread.quit()
         self.frame_grabber_thread.wait()
-        
+
         event.accept()
-        
+
 
 if __name__ == "__main__":
     app = Q.QApplication(sys.argv)
     window = VideoPlayerWidget()
     test_file = sys.argv[1]
-    window.set_file(test_file)                      
+    window.set_file(test_file)
     window.show()
-    sys.exit(app.exec_()) 
-
+    sys.exit(app.exec_())
--- examples/player.py
+++ examples/player.py
@@ -6,13 +6,15 @@ except NameError:
 
 import argparse
 import ctypes
+import struct, array
 import os
 import sys
 import pprint
 import time
 
 from qtproxy import Q
-from glproxy import gl
+import ModernGL as GL
+#from glproxy import gl
 
 import av
 
@@ -45,6 +47,7 @@ class PlayerGLWidget(Q.OpenGLWidget):
         while not self.frameCache:
             self.frameCache = next(self.demuxed).decode()
         return self.frameCache.pop(0)
+
     @Q.pyqtSlot()
     def onTimeout(self):
         print("received a timer event!")
@@ -52,35 +55,65 @@ class PlayerGLWidget(Q.OpenGLWidget):
         w = img.width
         h = img.height
         self.setImage(w,h,img.reformat(w,h,"rgb24"))
+
     def initializeGL(self):
         print('initialize GL')
-        fmt = self.format()
-        fmt.setSamples(4)
-        fmt.setMajorVersion(3)
-        fmt.setMinorVersion(3)
-        fmt.setProfile(Q.SurfaceFormat.CoreProfile)
-        self.setFormat(fmt)
-        gl.clearColor(0, 0, 0, 0)
-        gl.enable(gl.TEXTURE_2D)
-        # gl.texEnv(gl.TEXTURE_ENV, gl.TEXTURE_ENV_MODE, gl.DECAL)
-        self.tex_id = gl.genTextures(1)
-        gl.bindTexture(gl.TEXTURE_2D, self.tex_id)
-        gl.texParameter(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-        gl.texParameter(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-        print(('texture id', self.tex_id))
+        try:
+            GL.Init()
+            GL.Viewport(0, 0, self.width,self.height)
+            vert = GL.NewVertexShader("""
+#version 430
+in  vec2 a_position;
+out vec2 v_texcoord;
+void main(void) {
+    gl_Position = vec4(a_position, 0., 1.);
+    v_texcoord  = (a_position + vec2(1.,1.)) * .5;
+}""")
+            frag = GL.NewFragmentShader("""
+#version 430
+in vec2  v_texcoord;
+out vec4 f_fragcolor;
+uniform sampler2D u_sampler;
+void main(void) {
+    f_fragcolor = texture(u_sampler, v_texcoord);
+}""")
+            self.prog = GL.NewProgram([vert,frag])
+            self.iface = self.prog.interface
+            self.vbo = GL.NewVertexBuffer(struct.pack('8f', -1.,-1.,-1.,1.,1.,-1.,1.,1.))
+            self.vao = GL.NewVertexArray(self.prog,self.vbo,'2f',['a_position'])
+            self.tex = GL.NewTexture(self.img_width,self.img_height,bytes(self.img_width*self.img_height*3))
+            GL.UseTexture(self.tex)
+        except GL.Error as error:
+            print("Error: ",error)
+            exit(1)
+#            gl.clearColor(0, 0, 0, 0)
+#            gl.enable(gl.TEXTURE_2D)
+            # gl.texEnv(gl.TEXTURE_ENV, gl.TEXTURE_ENV_MODE, gl.DECAL)
+#            self.tex_id = gl.genTextures(1)
+            #gl.bindTexture(gl.TEXTURE_2D, self.tex_id)
+            #gl.texParameter(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+#            gl.texParameter(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+        print(('texture id', self.tex))
     def setImage(self,w,h,img):
-        print(("setting new image, size {}x{}".format(w,h)))
-        ptr = ctypes.c_void_p(img.planes[0].ptr)
-        gl.bindTexture(gl.TEXTURE_2D, self.tex_id)
-        gl.texImage2D(gl.TEXTURE_2D, 0, 3, w, h, 0, gl.RGB, gl.UNSIGNED_BYTE, ptr)
-        self.img_width  = w
-        self.img_height = h
-        self.update()
+        try:
+            self.prog
+            print(("setting new image, size {}x{}".format(w,h)))
+#        ptr = ctypes.c_void_p(img.planes[0].to_bytes())
+            self.tex = GL.NewTexture(w,h,img.planes[0].to_bytes())
+            GL.UseTexture(self.tex)
+#        gl.bindTexture(gl.TEXTURE_2D, self.tex_id)
+#        gl.texImage2D(gl.TEXTURE_2D, 0, 3, w, h, 0, gl.RGB, gl.UNSIGNED_BYTE, ptr)
+            self.img_width  = w
+            self.img_height = h
+            self.update()
+#            self.update()
+        except:
+            pass
     def resizeGL(self, w, h):
         print(('resize to', w, h))
         self.width  = w
         self.height = h
-        gl.viewport(0, 0, w, h)
+        GL.Viewport(0, 0, w, h)
         # gl.matrixMode(gl.PROJECTION)
         # gl.loadIdentity()
         # gl.ortho(0, w, 0, h, -10, 10)
@@ -92,24 +125,47 @@ class PlayerGLWidget(Q.OpenGLWidget):
         hratio /= dratio
         vratio /= dratio
         print('paint!')
-        gl.clear(gl.COLOR_BUFFER_BIT)
-        with gl.begin('polygon'):
-            gl.texCoord(0, 0); gl.vertex(-hratio,  vratio)
-            gl.texCoord(1, 0); gl.vertex( hratio,  vratio)
-            gl.texCoord(1, 1); gl.vertex( hratio, -vratio)
-            gl.texCoord(0, 1); gl.vertex(-hratio, -vratio)
+#        gl.clear(gl.COLOR_BUFFER_BIT)
+        GL.Clear(0,0,0,0)
+        if self.tex:
+            GL.UseTexture(self.tex)
+            GL.RenderTriangleStrip(self.vao,4)
+#            self.update()
+#        with gl.begin('polygon'):
+#            gl.texCoord(0, 0); gl.vertex(-hratio,  vratio)
+            #gl.texCoord(1, 0); gl.vertex( hratio,  vratio)
+            #gl.texCoord(1, 1); gl.vertex( hratio, -vratio)
+            #gl.texCoord(0, 1); gl.vertex(-hratio, -vratio)
+
+class Canvas(Q.QMainWindow):
+    def __init__(self,filename,format=None,parent=None):
+        super(self.__class__,self).__init__(parent)
+        self.widget = PlayerGLWidget(container=av.open(filename))
+        self.widget.setFixedWidth(WIDTH)
+        self.widget.setFixedHeight(HEIGHT)
+        self.widget.setSizePolicy(Q.SizePolicy.Expanding,Q.SizePolicy.Expanding)
+        self.setCentralWidget(self.widget)
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--format')
 parser.add_argument('path')
 args = parser.parse_args()
+fmt = Q.SurfaceFormat.defaultFormat()
+fmt.setVersion(4,5)
+fmt.setProfile(Q.SurfaceFormat.CoreProfile)
+fmt.setSamples(4)
+Q.SurfaceFormat.setDefaultFormat(fmt)
+
 app = Q.Application([])
 
-glwidget = PlayerGLWidget(container=av.open(args.path,format=args.format))
-glwidget.setFixedWidth(WIDTH)
-glwidget.setFixedHeight(HEIGHT)
-glwidget.setSizePolicy(Q.SizePolicy.Expanding,Q.SizePolicy.Expanding)
-glwidget.show()
-glwidget.raise_()
+glcanvas = Canvas(args.path,format=args.format)
+glcanvas.show()
+glcanvas.raise_()
+#glwidget = PlayerGLWidget(container=av.open(args.path,format=args.format))
+#glwidget.setFixedWidth(WIDTH)
+#glwidget.setFixedHeight(HEIGHT)
+#glwidget.setSizePolicy(Q.SizePolicy.Expanding,Q.SizePolicy.Expanding)
+#glwidget.show()
+#glwidget.raise_()
 
 #@timer.timeout.connect
 #def on_timeout(*args):
