879ca4626b77a935ea540db03a98951903a06978
gabriel d. karpman <gdkar@alum.mit.edu>
2017-04-11 21:38:53 -0400

    language_level=3

--- setup.py
+++ setup.py
@@ -623,6 +623,7 @@ class CythonizeCommand(Command):
                         c_string_type='str',
                         c_string_encoding='ascii',
                         embedsignature=True,
+                        language_level=3,
                     ),
                     build_dir='src',
                     include_path=ext.include_dirs,
